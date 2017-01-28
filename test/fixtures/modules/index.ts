f6105b9d74e6c4caaa65e6000ad90ea5f914a68d
ktsn <ktsn55@gmail.com>
2017-01-29 01:18:55 +0900

    Remove old test files

--- test/fixtures/modules/index.ts
+++
@@ -1,7 +0,0 @@
-import { create } from '../../../src'
-import Counter from './counter'
-import Todos from './todos'
-
-export default create()
-  .module('counter', Counter)
-  .module('todos', Todos)
--- test/fixtures/store.ts
+++
@@ -1,4 +0,0 @@
-import { store } from '../../src'
-import Root from './modules'
-
-export default store(Root)
